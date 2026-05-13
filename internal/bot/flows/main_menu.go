1e33c340cd8420f3416c43532d9695f3fbaadaea
dimatolpygin <barkinhoevh3@gmail.com>
2026-05-13 16:32:26 +0900

    Send bottom_menu reply keyboard on main menu entry

--- internal/bot/flows/main_menu.go
+++ internal/bot/flows/main_menu.go
@@ -4,5 +4,6 @@ import "context"
 
 func HandleMainMenu(ctx context.Context, fc *Context, d *Deps) {
 	_ = d.State.SetStep(ctx, fc.VkID, StepMainMenu)
+	_ = sendScreen(ctx, d, fc.VkID, "bottom_menu", ScreenOptions{})
 	_ = sendScreen(ctx, d, fc.VkID, "main_menu", ScreenOptions{})
 }
